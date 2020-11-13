d4e84873c6d2c940fc2b14c2d8bec46af27d927b
prajaktajad <72594375+prajaktajad@users.noreply.github.com>
2020-11-13 13:09:56 +0530

    Update AdminController

--- GMS/src/main/java/com/gms/controller/AdminController.java
+++ GMS/src/main/java/com/gms/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.gms.controller;
 
-
 import java.io.IOException;
 import java.util.List;
 
@@ -11,36 +10,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gms.model.DeptHead;
 import com.gms.model.Users;
 import com.gms.service.AdminService;
 import com.mysql.cj.protocol.x.SyncFlushDeflaterOutputStream;
 
-@ServletSecurity(
-		value = @HttpConstraint(
-				rolesAllowed = {"admin"}
-				)
-		)
-
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = { "admin" }))
 public class AdminController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	AdminService as = new AdminService();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String path = request.getPathInfo();
 
-		//list heads
-		if(path.equals("/listheads")) {			
+		//function for list all heads
+		if (path.equals("/listheads")) {
 			try {
-				List<Users> xyz =  as.getHeads();
-				request.setAttribute("heads", xyz);
+				List<DeptHead> user = as.getHeads();
+				request.setAttribute("heads", user);
 				request.getRequestDispatcher("/admin/admin-home.jsp").forward(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
-		//add new dept head
-		if(path.equals("/addhead")) {			
+		//function for adding new dept head
+		if (path.equals("/addhead")) {
 			try {
 				Users head = new Users();
 				head.setUsername(request.getParameter("username"));
@@ -49,62 +45,75 @@ public class AdminController extends HttpServlet {
 				head.setPhoneNo(request.getParameter("phoneno"));
 				head.setAddress(request.getParameter("address"));
 				head.setRole(request.getParameter("role"));
-				head.setDeptId(Integer.parseInt(request.getParameter("deptid")));
+				if (request.getParameter("dept").equals("water")) {
+					head.setDeptId(1);
+				} else if (request.getParameter("dept").equals("road")) {
+					head.setDeptId(2);
+				} else if (request.getParameter("dept").equals("electricity")) {
+					head.setDeptId(3);
+				} else {
+					head.setDeptId(4);
+				}
 				as.addHead(head);
-				response.sendRedirect("listheads");								
+				response.sendRedirect("listheads");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		//delete head
-		if(path.equals("/deletehead")) {			
+		//function for deleting department head
+		if (path.equals("/deletehead")) {
 			try {
 				as.deleteHead(request.getParameter("username"));
 				request.setAttribute("deletemsg", "head deleted successfully");
-				response.sendRedirect("listheads");								
+				response.sendRedirect("listheads");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-
-		if(path.equals("/edithead")) {			
+		//function for getting department head which has to update
+		if (path.equals("/gethead")) {
 			try {
 				Users user = as.getHead(request.getParameter("username"));
 				request.setAttribute("user", user);
-				request.getRequestDispatcher("/admin/update-head.jsp").forward(request, response);								
+				request.getRequestDispatcher("/admin/update-head.jsp").forward(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		if(path.equals("/updatehead")) {			
+		//function for updating department head
+		if (path.equals("/updatehead")) {
 			try {
 				System.out.println("controller");
 				Users user = new Users();
 				user.setUsername(request.getParameter("username"));
-				
-				System.out.println(request.getParameter("username"));
-				System.out.println(request.getParameter("email"));
-				System.out.println(request.getParameter("phoneno"));
-				System.out.println(request.getParameter("address"));
-				System.out.println(request.getParameter("role"));
-				System.out.println(request.getParameter("deptid"));
-				
+
 				user.setPassword(request.getParameter("password"));
 				user.setEmail(request.getParameter("email"));
 				user.setPhoneNo(request.getParameter("phoneno"));
 				user.setAddress(request.getParameter("address"));
 				user.setRole(request.getParameter("role"));
-				user.setDeptId(Integer.parseInt(request.getParameter("deptid")));
+				if (request.getParameter("dept").equals("water")) {
+					user.setDeptId(1);
+				} else if (request.getParameter("dept").equals("road")) {
+					user.setDeptId(2);
+				} else if (request.getParameter("dept").equals("electricity")) {
+					user.setDeptId(3);
+				} else {
+					user.setDeptId(4);
+				}
+
+				// user.setDeptId(Integer.parseInt(request.getParameter("deptid")));
 				as.updateHead(user);
-				response.sendRedirect("listheads");										
+				response.sendRedirect("listheads");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-	
+
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
