398835c4e33e400401cc6b2f785a41eb704ed1d2
prajaktajad <72594375+prajaktajad@users.noreply.github.com>
2020-11-18 20:57:55 +0530

    Update HeadTest.java
    Junit Testing for HeadService

--- GMS/src/test/java/com/gms/junit/HeadTest.java
+++ GMS/src/test/java/com/gms/junit/HeadTest.java
@@ -1,37 +1,37 @@
-/*
- * package com.gms.junit;
- * 
- * import static org.junit.jupiter.api.Assertions.*;
- * 
- * import org.junit.jupiter.api.Test;
- * 
- * import com.gms.service.HeadService;
- * 
- * class HeadTest {
- * 
- * HeadService service=new HeadService();
- * 
- * 
- * @Test void testGetComplaints() throws Exception {
- * assertNotNull(service.getComplaints("neeta")); }
- * 
- * @Test void testTransferComplaint() throws Exception {
- * 
- * assertEquals(1,service.transferComplaint(1)); }
- * 
- * @Test void testGetResolvedComplaints() throws Exception {
- * assertNotNull(service.getComplaints("neeta")); }
- * 
- * @Test void testGetDeptIdByDeptName() throws Exception {
- * assertEquals(2,service.getDeptIdByDeptName("road")); }
- * 
- * 
- * @Test void testUpdateDeptId() throws Exception {
- * assertEquals(1,service.updateDeptId(1, 3)); }
- * 
- * @Test void testTakeAction() throws Exception {
- * assertEquals(1,service.takeAction(2)); }
- * 
- * 
- * }
- */
+
+  package com.gms.junit;
+  
+  import static org.junit.jupiter.api.Assertions.*;
+  
+  import org.junit.jupiter.api.Test;
+  
+  import com.gms.service.HeadService;
+  
+  class HeadTest {
+  
+  HeadService service=new HeadService();
+  
+  
+  @Test void testGetComplaints() throws Exception {
+  assertNotNull(service.getComplaints("neeta")); }
+  
+  @Test void testTransferComplaint() throws Exception {
+  
+  assertEquals(1,service.transferComplaint(1)); }
+  
+  @Test void testGetResolvedComplaints() throws Exception {
+  assertNotNull(service.getComplaints("neeta")); }
+  
+  @Test void testGetDeptIdByDeptName() throws Exception {
+  assertEquals(2,service.getDeptIdByDeptName("road")); }
+  
+  
+  @Test void testUpdateDeptId() throws Exception {
+  assertEquals(1,service.updateDeptId(1, 3)); }
+  
+  @Test void testTakeAction() throws Exception {
+  assertEquals(1,service.takeAction(2)); }
+  
+  
+  }
+ 
