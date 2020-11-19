a4c2598ec3811e78bcbe953cf67ad8accc800d2e
snehaladk <72594376+snehaladk@users.noreply.github.com>
2020-11-19 10:55:25 +0530

    Add files via upload
    controller

--- GMS/src/main/java/com/gms/controller/CitizenController.java
+++ GMS/src/main/java/com/gms/controller/CitizenController.java
@@ -1,65 +1,145 @@
 package com.gms.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.List;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.gms.model.Complaint;
+import com.gms.model.ComplaintObject;
 import com.gms.model.Users;
 import com.gms.service.AdminService;
 import com.gms.service.CitizenService;
 
-/**
- * Servlet implementation class CitizenController
- */
+///@ServletSecurity(value = @HttpConstraint(rolesAllowed = { "citizen" }))
+@MultipartConfig(maxFileSize = 16177215)//upload upto 16MB 
 public class CitizenController extends HttpServlet {
+	public static final Logger logger = LogManager.getLogger(CitizenController.class.getName());
 	private static final long serialVersionUID = 1L;
 	CitizenService cs = new CitizenService();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getPathInfo();
 		
-		//HttpSession session1=request.getSession();
+		//creating Http session object for getting citizen name
 		HttpSession session=request.getSession();
-		System.out.println("Citizen name::"+session.getAttribute("citizenname"));
 		String name=(String) session.getAttribute("citizenname");
-		//System.out.println("username"+username);
 		int id=0;
 		try {
+		
 			id = cs.getId((String)session.getAttribute("citizenname"));
-			System.out.println("userId1::::"+id);
-		} catch (Exception e1) {
-			e1.printStackTrace();
+		} catch (Exception e) {
+			logger.error("Fail to get User id of user",e);
 		}
-		System.out.println("userId2::::"+id);
-		//register new complain 
-		if(path.equals("/registercomplaint")) {			
+		
+		
+		
+		//list previous complaints of citizen
+		if(path.equals("/listcomplaints")) {	
 			try {
-				
-				Complaint complaint = new Complaint();
-				complaint.setDescription(request.getParameter("description"));
-				complaint.setUserRemark(request.getParameter("remark"));
-				complaint.setStatus(request.getParameter("status"));
-				complaint.setDeptId(Integer.parseInt(request.getParameter("deptid")));
-				complaint.setUserId(id);
-				complaint.setStatus("pending");
-				complaint.setHeadRemark("pending");
-				cs.registerComplaint(complaint);
-				//request.getRequestDispatcher("/citizen/citizen-home.jsp").forward(request, response);
-				response.sendRedirect("citizen-home.jsp");								
+				logger.info("Citizen User checkig our registered complaints");
+				//calling getComplaints function from CitizenService
+				List<ComplaintObject> complaints =  cs.getComplaints(id);
+				request.setAttribute("complaints", complaints);
+				request.getRequestDispatcher("/citizen/view-complaints.jsp").forward(request, response);
 			} catch (Exception e) {
-				e.printStackTrace();
+				logger.error("Not getting lsit of complaints to citizen user",e);
 			}
 		}
 		
+		//register new citizen
+		if(path.equals("/registercitizen")) {
+			try {
+				logger.info("User Register as a Citizen");
+				Users user=new Users();
+				user.setUsername(request.getParameter("username"));
+				user.setPassword(request.getParameter("password"));
+				user.setEmail(request.getParameter("email"));
+				user.setPhoneNo(request.getParameter("phoneno"));
+				user.setAddress(request.getParameter("address"));
+				user.setRole("citizen");
+				//calling registerCitizen function from CitizenService
+				cs.registerCitizen(user);
+                HttpSession session1 = request.getSession();
+                session1.setAttribute("registerdmsg", "User registered  successfully");
+
+				response.sendRedirect("/GMS/login.jsp");	
+			} catch (Exception e) {
+				logger.error("Citizen registration failed",e);
+				
+			}
+		}	
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+      String path = request.getPathInfo();
+
+      //creating Http session object for getting citizen name
+      HttpSession session=request.getSession();
+      String name=(String) session.getAttribute("citizenname");
+      int id=0;
+      try {
+            id = cs.getId((String)session.getAttribute("citizenname"));
+      } catch (Exception e1) {
+            logger.error("session failed...",e1);
+      }
+
+      //register new complaint 
+      if(path.equals("/registercomplaint")) {               
+            try {
+
+                  Complaint complaint = new Complaint();
+                  complaint.setDescription(request.getParameter("description"));
+                  complaint.setUserRemark(request.getParameter("remark"));
+                  complaint.setStatus(request.getParameter("status"));
+                  if(request.getParameter("dept").equals("water")){
+                        complaint.setDeptId(1);
+                  }
+                  else if(request.getParameter("dept").equals("road")){
+                        complaint.setDeptId(2);
+                  }
+                  else if(request.getParameter("dept").equals("electricity")){
+                        complaint.setDeptId(3);
+                  }
+                  else {
+                        complaint.setDeptId(4);
+                  }
+                  complaint.setUserId(id);
+                  complaint.setStatus("pending");
+                  complaint.setHeadRemark("pending");
+
+
+                  Part filePart = request.getPart("screenshot"); 
+                  String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString()+"/"; 
+                  InputStream fileContent = filePart.getInputStream();
+                  complaint.setScreenshot(fileContent);    
+                  System.out.println(fileName);
+
+                  //calling registerComplaint function from CitizenService
+                  cs.registerComplaint(complaint);
+                  request.getRequestDispatcher("/citizen/citizen-home.jsp").forward(request, response);                                        
+            } catch (Exception e) {
+                  logger.error("complaint registration failed...",e);
+
+            }
+      }
+
+      doGet(request, response);
+}
+
 
 }
--- GMS/src/main/java/com/gms/controller/HeadController.java
+++ GMS/src/main/java/com/gms/controller/HeadController.java
@@ -4,42 +4,101 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.gms.model.Complaint;
+import com.gms.model.ComplaintObject;
 import com.gms.model.Users;
 import com.gms.service.AdminService;
 import com.gms.service.HeadService;
 
-/**
- * Servlet implementation class HeadController
- */
+//@ServletSecurity(value = @HttpConstraint(rolesAllowed = { "head" }))
 public class HeadController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+                public static final Logger logger = LogManager.getLogger(HeadController.class.getName());
+                private static final long serialVersionUID = 1L;
 
-	HeadService hs = new HeadService();
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getPathInfo();
-		//list heads
-		if(path.equals("/listcomplaints")) {			
-			try {
-				List<Complaint> complaints =  hs.getComplaints();
-				request.setAttribute("complaints", complaints);
-				request.getRequestDispatcher("head-home.jsp").forward(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+                HeadService hs = new HeadService();
 
-	}
+                protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                                                throws ServletException, IOException {
+                                String path = request.getPathInfo();
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+                                //creating Http session object for getting department head name
+                                HttpSession session = request.getSession();
+                                String headName = (String) session.getAttribute("headName");
 
+                                //list all complaints of particular department head
+                                if (path.equals("/listcomplaints")) {
+                                                try {
+                                                                logger.info("Departmental Head Checking all complaints related to own Department");
+                                                                //calling getComplaints function from HeadService(pending complaints)
+                                                                List<ComplaintObject> complaints = hs.getComplaints(headName);
+                                                                request.setAttribute("complaints", complaints);
+                                                                //calling getResolvedComplaints function from HeadService(resolved complaints)
+                                                                List<ComplaintObject> resolvedComplaints = hs.getResolvedComplaints(headName);
+                                                                request.setAttribute("resolvedComplaints", resolvedComplaints);
+                                                                request.getRequestDispatcher("/head/show-complaints.jsp").forward(request, response);
+                                                } catch (Exception e) {
+                                                	logger.error("Not getting complaints to respective department",e);
+                                                }
+                                }
+                                
+                                //redirect to transfer complaint page
+                                if (path.equals("/transferComplaintpage")) {
+                                                try {
+                                                                logger.info("Try to transfer complaint");
+                                                                session.setAttribute("cmpid", request.getParameter("cid"));
+                                                                int cmpId = Integer.parseInt((String) session.getAttribute("cmpid"));
+                                                                request.getRequestDispatcher("/head/transfercomplaint.jsp").forward(request, response);
+                                                } catch (Exception e) {
+                                                	logger.error("Fail to redirect transfer complaint page",e);
+                                                }
+
+                                }
+                                
+                                //transfer complaint to another department
+                                if (path.equals("/transferComplaint")) {
+                                                int cmpId = Integer.parseInt((String) session.getAttribute("cmpid"));
+                                                try {
+                                                                logger.info("Head Transfering complete to the respective Department");
+                                                                //calling getDeptIdByDeptName function from HeadService
+                                                                System.out.println("deptname::"+request.getParameter("depts"));
+                                                                int id = hs.getDeptIdByDeptName(request.getParameter("depts"));
+                                                //            System.out.println("dept id="+id);
+                                                                //System.out.println("cmp id="+cmpId);
+                                                                
+                                                                //calling updateDeptId function from HeadService
+                                                                hs.updateDeptId(id, cmpId);
+                                                } catch (Exception e) {
+                                                	logger.error("fail to transfer complaint",e);
+                                                }
+                                                response.sendRedirect("listcomplaints");
+                                }
+
+                                //take action on complaint
+                                if (path.equals("/takeAction")) {
+                                                int cid = Integer.parseInt((String) request.getParameter("cid"));
+                                                try {
+                                                                logger.info("Head taking some action on respective complaint");
+                                                                //calling takeAction function from HeadService
+                                                                hs.takeAction(cid);
+                                                } catch (Exception e) {
+                                                	logger.error("Fail to take action on Complaint",e);
+                                                }
+                                                response.sendRedirect("listcomplaints");
+                                }
+                }
+
+                protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                                                throws ServletException, IOException {
+                                doGet(request, response);
+                }
 }
