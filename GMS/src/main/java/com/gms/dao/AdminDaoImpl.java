e873903a1fc1bf3966f0144ca821342b1e0e35a2
snehaladk <72594376+snehaladk@users.noreply.github.com>
2020-11-19 10:56:09 +0530

    Add files via upload
    Dao

--- GMS/src/main/java/com/gms/dao/AdminDaoImpl.java
+++ GMS/src/main/java/com/gms/dao/AdminDaoImpl.java
@@ -6,54 +6,63 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.gms.model.DeptHead;
 import com.gms.model.Users;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AdminDaoImpl implements IAdminDao {
-
+	public static final Logger logger = LogManager.getLogger(AdminDaoImpl.class.getName());
+	//function for adding new department head
 	public int addHead(Users head) throws Exception {
-
-		System.out.println("into addhead");
-		String sql = "insert into users (username,password,email,phoneno,address,role,deptid) values(?, ?, ?,?,?,?,?)";
-		System.out.println(sql);
-		Connection connection = DbUtil.getConnection();
-		PreparedStatement ps = connection.prepareStatement(sql);
-
+		String sql = "insert into users (username,password,email,phoneno,address,role,deptid) values(?,?,?,?,?,?,?)";
+		try(Connection connection = DbUtil.getConnection();PreparedStatement ps = connection.prepareStatement(sql))
+		{
+		
+		
 		ps.setString(1, head.getUsername());
 		ps.setString(2, head.getPassword());
 		ps.setString(3, head.getEmail());
 		ps.setString(4, head.getPhoneNo());
 		ps.setString(5, head.getAddress());
-		ps.setString(6, head.getRole());
+		ps.setString(6, "head");
 		ps.setInt(7, head.getDeptId());
-
+		logger.info("Admin Adding head into the Database");
 		return ps.executeUpdate();
+		}
 	}
 
-	public List<Users> getHeads() throws Exception {
-		String sql = "select * from users";
+	//function for getting all department heads
+	public List<DeptHead> getHeads() throws Exception {
+		String sql = "select u.username, u.email, u.phoneno, u.address, d.deptname, u.role from users u join department d on u.deptid=d.deptid";
 		Connection connection = DbUtil.getConnection();
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ResultSet rs = ps.executeQuery();
-
-		List<Users> heads = new ArrayList<Users>();
+		List<DeptHead> heads = new ArrayList<DeptHead>();
 		while (rs.next()) {
+			//if users has head role then only add into temporary head list
 			if (rs.getString("role").equals("head")) {
-				heads.add(new Users(rs.getInt(1), rs.getString(2), rs.getString(3), 
-						rs.getString(4), rs.getString(5),
-						rs.getString(6), rs.getString(7), rs.getInt(8)));
+				heads.add(new DeptHead(rs.getString(1), rs.getString(2), rs.getString(3), 
+						rs.getString(4), rs.getString(5)));
 			}
 		}
+		logger.info("getting Heads");
 		return heads;
-
 	}
+	
+	//function for deleting department head
 	public int deleteHead(String username) throws Exception{
 		String sql = "delete from users where username = ?";
 		Connection connection = DbUtil.getConnection();
 		PreparedStatement ps = connection.prepareStatement(sql);		
-		ps.setString(1, username);			
+		ps.setString(1, username);		
+		logger.info("Deleting Head from Database");
 		return ps.executeUpdate();
 	}
 
+	//function for getting department head  which is needed for updating head
 	public Users getHead(String username) throws Exception {
 		String sql = "select * from users where username = ?";
 		Connection connection = DbUtil.getConnection();
@@ -66,30 +75,23 @@ public class AdminDaoImpl implements IAdminDao {
 					rs.getString(4),rs.getString(5),rs.getString(6),
 					rs.getString(7),rs.getInt(8));
 		}
+		logger.info("getting department head  which is needed for updating head");
 		return user;
 	}
 
+	//function for updating department head
 	public int updateHead(Users user) throws Exception {
 		String sql = "update users set password = ? , email=? , phoneno=? , address=? , role=? , deptid=? where username = ?";
-		Connection connection = DbUtil.getConnection();
-		System.out.println(user.getPassword());
-		System.out.println(user.getEmail());
-		System.out.println(user.getPhoneNo());
-		System.out.println(user.getAddress());
-		System.out.println(user.getRole());
-		System.out.println(user.getUsername());
-		
+		Connection connection = DbUtil.getConnection();		
 		PreparedStatement ps = connection.prepareStatement(sql);
-		
 		ps.setString(1, user.getPassword());
 		ps.setString(2, user.getEmail());
 		ps.setString(3, user.getPhoneNo());
 		ps.setString(4, user.getAddress());
-		ps.setString(5, user.getRole());
+		ps.setString(5, "head");
 		ps.setInt(6, user.getDeptId());
 		ps.setString(7, user.getUsername());
-		
+		logger.info("Updating Department Head");
 		return ps.executeUpdate();
 	}
-	
 }
--- GMS/src/main/java/com/gms/dao/DbUtil.java
+++ GMS/src/main/java/com/gms/dao/DbUtil.java
@@ -1,64 +1,49 @@
 package com.gms.dao;
 
-import java.io.FileReader;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.Properties;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DbUtil {
-	public static String dbUrl="jdbc:mysql://localhost:3306/gms-db" ;
-	public static String dbUser="root" ;
-	public static String dbPassword="cybage@123" ;
-	/*
-	 * 
-	 * dbURL=jdbc:mysql://localhost:3306/gms-db
-dbUser=root
-dbPassword=cybage@123
-	static {
-		try{
-			FileReader fr = new FileReader("app.properties");	//reading file
-			Properties props = new Properties();				//properties to convert data into key value
-			props.load(fr);
-			dbUrl = props.getProperty("dbUrl");
-			dbUser = props.getProperty("dbUser");
-			dbPassword = props.getProperty("dbPassword");
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	*/
+      
+      public static String dbURL;
+      public static String dbUser ;
+      public static String dbPassword ;
+      static {
+            try {
+                  ClassLoader classloader = Thread.currentThread().getContextClassLoader();             
+                  InputStream is = classloader.getResourceAsStream("app.properties");
 
-	
-	//with connection pool
-		/*public static Connection getConnection() throws Exception{
-			Connection con = DriverManager.getConnection();
-			return con;
-		}
-*/
+                  Properties props = new Properties();
+                  props.load(is);
+                  dbURL = props.getProperty("dbURL");
+                  dbUser = props.getProperty("dbUser");
+                  dbPassword = props.getProperty("dbPassword");
+                  
+            } catch (Exception e) {
+                  e.printStackTrace();
+            }           
+      }
 
-	//without connection pool
-		public static Connection getConnection() throws Exception{
-		Class.forName("com.mysql.cj.jdbc.Driver");		//class will be loaded automatically (thin driver)
-			Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-		return con;
-		}
+      //with connection pool
+      public static Connection getConnection() throws Exception{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = getDataSource().getConnection();
+            return con;
+      }
 
-	//getting connection from connection pool
-	/*public static Connection getConnection() throws Exception{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection();
-		return con;
-	}
-	private static BasicDataSource getDataSource(){
-		BasicDataSource ds = new BasicDataSource();
-		ds.setUrl(dbUrl);
-		ds.setUsername(dbUser);
-		ds.setPassword(dbPassword);
-		ds.setMinIdle(5);
-		ds.setMaxIdle(10);
-		ds.setMaxOpenPreparedStatements(100);
-		return ds;		
-	}*/
+      private static BasicDataSource getDataSource()
+      {        
+            BasicDataSource ds = new BasicDataSource();
+            ds.setUrl(dbURL);
+            ds.setUsername(dbUser);
+            ds.setPassword(dbPassword);
+            ds.setMinIdle(5);
+            ds.setMaxIdle(10);
+            ds.setMaxOpenPreparedStatements(100); 
+            return ds;
+      }
+
 }
